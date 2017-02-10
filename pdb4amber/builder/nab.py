e76c82b1358ff697442b1d9ab31b361e843c68fc
Hai Nguyen <hainm.comp@gmail.com>
2017-02-10 11:42:40 -0500

    update nab

--- pdb4amber/builder/nab.py
+++ pdb4amber/builder/nab.py
@@ -10,9 +10,18 @@ def run(command):
     with open(nabin, 'w') as fh:
         fh.write(command)
     nab_bin = which('nab')
+    if not nab_bin:
+        raise FileNotFoundError('require nab')
     prefix = os.path.abspath(
         os.path.join(os.path.dirname(nab_bin), '..'))
     os.environ['AMBERHOME'] = prefix
+    my_ld_path = os.path.join(prefix, 'lib')
+    ld_paths = os.getenv('LD_LIBRARY_PATH')
+
+    if ld_paths:
+        os.environ['LD_LIBRARY_PATH'] = my_ld_path + ':' + ld_paths
+    else:
+        os.environ['LD_LIBRARY_PATH'] = my_ld_path
     build_command = [
             nab_bin,
             nabin,
